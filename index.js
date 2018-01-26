bd582f5bd045b01d9d9dd0ada4262634f8bd0afd
Robert <robert.pastor0691@orange.fr>
2018-01-26 22:39:13 +0100

    project and version selection

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 "use strict";
 
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 
 var app = express();
+var server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  console.log(req.hostname);
+  //console.log(req.hostname);
   res.render('pages/index');
 });
+app.get('/klocproject', function (req, res) {
+  res.render('pages/klocproject');
+});
+app.get('/about', function (req, res) {
+  res.render('pages/about');
+});
+app.use(bodyParser.json()); // to support JSON bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+app.post('/form', function (req, res) {
+  console.log('selected project is= ' + req.body.optradio);
+  console.log('selected version is= ' + req.body.selectVersion);
+  res.send(req.body.optradio);
+});
+
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+  console.log('Listening on port= ' + String(PORT));
 });
